4b0bae223a3590ed0286f804a767bc3a5b1222f3
CzarProCoder <juliusczar36@gmail.com>
2023-10-08 14:22:17 +0300

    Update script as per pycodestyle

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -3,13 +3,14 @@
 Module containing function the converts a float value into a string
 '''
 
+
 def to_str(n: float) -> str:
     '''
     Function that converts a float into a string
 
     Args:
         n (float): Value to be converted into a string
-    
+
     Returns:
         string: String format of the float
     '''
