3db081117f81a150758cb3155b5366bf6333c388
CzarProCoder <juliusczar36@gmail.com>
2023-10-08 14:20:17 +0300

    Update module and functions comments

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-
+'''
+Module containing function tha converts a float into an integer
+'''
 import math
 
 
 def floor(n: float) -> int:
     '''
     Function that calculates the floor of a number
+
+    Args:
+        n (float): A float value to be converted into an integer
     '''
     return math.floor(n)
--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
+'''
+Module containing function the converts a float value into a string
+'''
 
 def to_str(n: float) -> str:
     '''
     Function that converts a float into a string
+
+    Args:
+        n (float): Value to be converted into a string
+    
+    Returns:
+        string: String format of the float
     '''
     return str(n)
