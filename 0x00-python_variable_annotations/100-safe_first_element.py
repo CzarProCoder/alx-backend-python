0af5c781928c9573edc53fd9396946f981bca721
CzarProCoder <juliusczar36@gmail.com>
2023-10-08 14:28:06 +0300

    Update module & function comments

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
+'''
+Module to calculate the length of different type of elements passed as a list
+'''
 
 from typing import Any, Union, Sequence
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """Return the first element of a list or None if the list is empty."""
+    '''
+    Return the first element of a list or None if the list is empty.
+
+    Args:
+        A list containing elements of different types
+    '''
     if lst:
         return lst[0]
     else:
