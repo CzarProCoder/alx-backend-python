5ee8c430adec73f197d09b9fe9247698d3217949
CzarProCoder <juliusczar36@gmail.com>
2023-10-30 23:03:01 +0300

    Implement class and method to test requests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,9 @@
 Test script for utils.py
 '''
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,3 +28,31 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    '''
+    Class to test get_json function
+    '''
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+        ])
+    def test_get_json(self, url, payload):
+        '''
+        Function to test get json
+        '''
+        class Mocked(Mock):
+            """
+            class that inherits from Mock
+            """
+
+            def json(self):
+                """
+                json returning a payload
+                """
+                return payload
+
+        with patch("requests.get") as MockClass:
+            MockClass.return_value = Mocked()
+            self.assertEqual(get_json(url), payload)
