7caf5a1f762f19d87498652a62803cafed55a1c2
CzarProCoder <juliusczar36@gmail.com>
2023-10-30 15:47:13 +0300

    Add test script for acces nested map

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''
+Test script for utils.py
+'''
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    '''
+    Test class for utils
+    '''
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ])
+    def test_utils(self, nested_map, pathP, expected):
+        '''
+        Test function for access_nested_map
+        '''
+        self.assertEqual(access_nested_map(nested_map, pathP), expected)
